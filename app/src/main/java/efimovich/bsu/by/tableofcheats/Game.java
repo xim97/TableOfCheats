c18793700d3a3fb114b95daa0e99423ca991ae69
Maxim Yefimovich <xim97@mail.ru>
2017-11-17 23:36:08 +0300

    Added Game class

---
+++ app/src/main/java/efimovich/bsu/by/tableofcheats/Game.java
@@ -0,0 +1,43 @@
+package efimovich.bsu.by.tableofcheats;
+
+public class Game {
+    private String name;
+    private int yearOfRelease;
+    private boolean isFavorite;
+
+    public Game(){
+        name = "asdf";
+        yearOfRelease = 1000;
+        isFavorite = false;
+    }
+
+    public Game(String name, int yearOfRelease, boolean isFavorite){
+        this.name = name;
+        this.yearOfRelease = yearOfRelease;
+        this.isFavorite = isFavorite;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
